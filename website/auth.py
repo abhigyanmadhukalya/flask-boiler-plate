adee057711f32ffe1d8f9230229c1eb373b9f001
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2022-08-23 21:52:20 +0530

    Continuing to make the boiler plate

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return '<h1>Logout Page</h1>'
 
-@auth.route('/signup')
+@auth.route('/signup',  methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email is invalid. Please try again.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be longer than 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
     return render_template("signup.html")
